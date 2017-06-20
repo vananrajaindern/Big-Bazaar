09e6adb43e0d9053d73464c52c9ccb0ab374b4c7
ngerald51 <starhubber81@gmail.com>
2017-06-20 17:34:48 +0800

    Intial Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+    def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :firstname, :last_name, :provider, :uid])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  #devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  #devise_scope :user do
+  #delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  #end
+
 end
